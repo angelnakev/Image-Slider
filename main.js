fd729812ed48f70fe39d45d302bce3c3b2b4f646
angelnakev <angelnakev@outlook.com>
2023-02-12 15:55:45 +0100

    pulled from DOM, vars set for autoScroll, IntervalTime and slideInterval

---
+++ main.js
@@ -0,0 +1,9 @@
+const slide = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('#next');
+const prevBtn = document.querySelector('#prev');
+
+const autoScroll = false;
+
+const intervalTime = 4000;
+
+let slideInterval;
