a335db757b5368412c0a17664fa68299664e9d58
angelnakev <angelnakev@outlook.com>
2023-02-12 16:03:09 +0100

    nextSlide and prevSlide functions added

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-const slide = document.querySelectorAll('.slide');
-const nextBtn = document.querySelector('#next');
-const prevBtn = document.querySelector('#prev');
+const slide = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector("#next");
+const prevBtn = document.querySelector("#prev");
 
 const autoScroll = false;
 
 const intervalTime = 4000;
 
 let slideInterval;
+
+const nextSlide = () => {
+  const current = document.querySelector(".current");
+
+  current.classList.remove("current");
+
+  if (current.nextElementSibling) {
+    current.nextElementSibling.classList.add(".current");
+  } else {
+    slide[0].classList.add("current");
+  }
+
+  setTimeout(() => current.classList.remove('.current'));
+};
+
+const prevSlide = () => {
+  const current = document.querySelector(".current");
+
+  current.classList.remove("current");
+
+  if (current.prevElementSibling) {
+    current.prevElementSibling.classList.add(".current");
+  } else {
+    slide[slide.length - 1].classList.add("current");
+  }
+
+  setTimeout(() => current.classList.remove('.current'));
+};
